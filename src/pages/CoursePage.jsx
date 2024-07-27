44e5a2592347eef524b325bed214f116ea96beda
ludevique <makamludovique@gmail.com>
2024-07-27 16:28:23 -0700

    axios function implemented

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+//-----------------import axios that will help me to get my data----------
+import axios from 'axios'
 
 const CoursePage = () => {
-    const handleClick = () => {}
+    const [courses, setCourses] = useState([]);
+
+    //use our useEffect hook to perform side effect in our components
+    useEffect(() => {
+      const myData = async () => {
+        try {
+          const result = await axios.get('/course');
+          //
+          setCourses(result.data)
+          
+        }catch (error) {
+            console.error('ran into an error while fectching course data',error)
+        };
+      };
+      //call my function
+      myData();
+    }, [])
+
   return (
     <div>
         <Link to='/course'>
